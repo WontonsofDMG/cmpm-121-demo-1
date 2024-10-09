55027ea72f460e66b15e520c186058cfbfdb75a3
Mitchell Tai <mitchelltai221@gmail.com>
2024-10-09 13:43:19 -0700

    Step 1 added a button

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My alright game";
+const gameName = "My okay game";
 document.title = gameName;
+function addrandomButton() {
 
+    const button = document.createElement("button");
+
+    button.innerText = "Button!";
+
+    button.style.fontSize = "20px";
+    button.style.padding = "10px 20px";
+    button.style.cursor = "pointer";
+
+    document.body.appendChild(button);
+}
+addrandomButton();
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
